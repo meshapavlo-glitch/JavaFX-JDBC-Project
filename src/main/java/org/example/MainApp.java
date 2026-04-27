fd6ce305ee14e5b50ecec1ecde3c3898b9fc1e50
meshapavlo-glitch <mesha.pavlo@student.sumdu.edu.ua>
2026-04-27 20:47:30 +0300

    Implement JavaFX application for Movie Manager

--- src/main/java/org/example/MainApp.java
+++ src/main/java/org/example/MainApp.java
@@ -1,1 +1,21 @@
+package org.example;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Movie Manager CRUD");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
